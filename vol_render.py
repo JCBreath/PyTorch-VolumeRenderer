d6a83c49edf75cfc08b415ff0b6717ac394b6e9a
Siyuan Yao <jim.charles.yao@gmail.com>
2023-03-09 05:33:20 -0500

    Fix camera view up

--- vol_render.py
+++ vol_render.py
@@ -5,20 +5,93 @@ import numpy as np
 import math
 from skimage.io import imread, imsave
 
-def sample_from_3dgrid(grid, coordinates):
-    """
-    Expects coordinates in shape (batch_size, num_points_per_batch, 3)
-    Expects grid in shape (1, channels, H, W, D)
-    (Also works if grid has batch size)
-    Returns sampled features of shape (batch_size, num_points_per_batch, feature_channels)
-    """
-    batch_size, n_coords, n_dims = coordinates.shape
-    sampled_features = torch.nn.functional.grid_sample(grid.expand(batch_size, -1, -1, -1, -1),
-                                                       coordinates.reshape(batch_size, 1, 1, -1, n_dims),
-                                                       mode='bilinear', padding_mode='zeros', align_corners=False)
-    N, C, H, W, D = sampled_features.shape
-    sampled_features = sampled_features.permute(0, 4, 3, 2, 1).reshape(N, H*W*D, C)
-    return sampled_features
+class VolumeRenderer(nn.Module):
+    def __init__(self, sr=1.0, res=512, projection='perspective'):
+        self.sr = sr
+        self.res = res
+        self.camera = torch.FloatTensor([3.0,3.0,3.0])
+        self.center = torch.FloatTensor([0.0,0.0,0.0])
+        self.up = torch.FloatTensor([0,1,0])
+        self.projection = projection
+
+    def get_rays(self):
+        camera_direction = (self.center - self.camera) / torch.linalg.norm(self.center - self.camera)
+        if self.projection == 'perspective':
+            screen = self.camera + camera_direction
+        else:
+            screen = self.camera
+        screen_right = torch.cross(camera_direction, self.up)
+        screen_right = screen_right / torch.linalg.norm(screen_right)
+        screen_up = torch.cross(screen_right, camera_direction)
+        screen_up = screen_up / torch.linalg.norm(screen_up)
+        xs = torch.linspace(-0.5, 0.5, steps=self.res)
+        ys = torch.linspace(0.5, -0.5, steps=self.res)
+        x, y = torch.meshgrid(xs, ys, indexing='xy')
+        self.save_image('/mnt/g/vis_data/x.png',x+0.5)
+        self.save_image('/mnt/g/vis_data/y.png',y+0.5)
+        print((x.unsqueeze(-1)*screen_right).size())
+        ray_targets = screen.expand(1,1,3).repeat(self.res,self.res,1) + x.unsqueeze(-1)*screen_right+y.unsqueeze(-1)*screen_up
+        if self.projection == 'perspective':
+            ray_origins = torch.FloatTensor(self.camera).expand(1,1,3).repeat(self.res,self.res,1)
+            ray_directions = (ray_targets - ray_origins) / torch.linalg.norm((ray_targets - ray_origins),dim=-1,keepdim=True)
+        else:
+            ray_origins = ray_targets
+            ray_directions = camera_direction.expand(1,1,3).repeat(self.res,self.res,1)
+
+
+        return ray_origins, ray_directions
+    
+    def render(self, data):
+        ray_origins, ray_directions = self.get_rays()
+        ray_origins = ray_origins.cuda()
+        ray_directions = ray_directions.cuda()
+
+        inv_ray_directions = 1.0 / ray_directions
+        dx = torch.abs(inv_ray_directions[:,:,0]/128.0)
+        dy = torch.abs(inv_ray_directions[:,:,1]/128.0)
+        dz = torch.abs(inv_ray_directions[:,:,2]/128.0)
+        dt = torch.cat((dx.unsqueeze(-1),dy.unsqueeze(-1),dz.unsqueeze(-1)),dim=-1)
+        dt, _ = torch.min(dt,dim=-1,keepdim=True)
+        print(dt.min(),dt.max())
+
+        rgbs = torch.zeros((self.res,self.res,1)).cuda()
+        alpha = torch.zeros((self.res,self.res,1)).cuda()
+
+        s = torch.zeros((self.res,self.res,1)).cuda()
+        while s.max() < 10:
+            sample_coordinates = ray_origins + ray_directions * s
+            values = self.sample_from_3dgrid(data, sample_coordinates)
+            if rgbs.sum() != 0 and values.max() == 0:
+                break
+            density = -(alpha-1.0) * values
+            rgbs += values * density * (values > 0.1)
+            alpha += density * (values > 0.1)
+            alpha = alpha.clip(0,1)
+            s += dt
+
+        return rgbs
+
+    def save_image(self, path, img):
+        img = img.cpu().numpy()
+        img = img * 255
+        img = img.astype(np.uint8)
+        imsave(path, img)
+        
+
+    def sample_from_3dgrid(self, grid, coordinates):
+        """
+        Expects coordinates in shape (batch_size, num_points_per_batch, 3)
+        Expects grid in shape (1, channels, H, W, D)
+        (Also works if grid has batch size)
+        Returns sampled features of shape (batch_size, num_points_per_batch, feature_channels)
+        """
+        batch_size, n_coords, n_dims = coordinates.shape
+        sampled_features = torch.nn.functional.grid_sample(grid.expand(batch_size, -1, -1, -1, -1),
+                                                        coordinates.reshape(batch_size, 1, 1, -1, n_dims),
+                                                        mode='bilinear', padding_mode='zeros', align_corners=False)
+        N, C, H, W, D = sampled_features.shape
+        sampled_features = sampled_features.permute(0, 4, 3, 2, 1).reshape(N, H*W*D, C)
+        return sampled_features
 
 
 def transferFunction(x):
@@ -68,6 +141,12 @@ data = np.fromfile('/mnt/g/vis_data/vorts/global_normalized/vorts-0001.iw', dtyp
 # data = np.fromfile('/mnt/g/vis_data/global_normalized/tornado-0001.iw', dtype = '<f')
 data = data.reshape(128,128,128)
 data = torch.FloatTensor(data).unsqueeze(0).unsqueeze(0)
+data = data.cuda()
+
+renderer = VolumeRenderer()
+img = renderer.render(data)
+renderer.save_image('/mnt/g/vis_data/test.png',img)
+exit()
 
 res = 512
 
